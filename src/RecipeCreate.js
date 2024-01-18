12b99e6b24201bfd46b5ad88b14051d2e3b290b3
Katrina Pingryn <katep@indeed.com>
2024-01-18 15:25:19 -0600

    add recipe app to github

---
+++ src/RecipeCreate.js
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+
+function RecipeCreate({ createRecipe }) {
+  const initialFormState = {
+    name: "",
+    cuisine: "",
+    photo: "",
+    ingredients: "",
+    preparation: "",
+  };
+
+  const [formData, setFormData] = useState({ ...initialFormState });
+  const handleChange = ({ target }) => {
+    const value = target.value;
+    setFormData({
+      ...formData,
+      [target.name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    createRecipe(formData);
+    // Clear form fields after recipe created
+    setFormData({ ...initialFormState });
+  };
+
+  return (
+    <form name="create" onSubmit={handleSubmit}>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <label htmlFor="name">
+                <input
+                  id="name"
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  onChange={handleChange}
+                  value={formData.name}
+                />
+              </label>
+            </td>
+            <td>
+              <label htmlFor="cuisine">
+                <input
+                  id="cuisine"
+                  type="text"
+                  name="cuisine"
+                  placeholder="Cuisine"
+                  onChange={handleChange}
+                  value={formData.cuisine}
+                />
+              </label>
+            </td>
+            <td>
+              <label htmlFor="photo">
+                <input
+                  id="photo"
+                  type="url"
+                  name="photo"
+                  placeholder="Photo URL"
+                  onChange={handleChange}
+                  value={formData.photo}
+                />
+              </label>
+            </td>
+            <td>
+              <label htmlFor="ingredients">
+                <textarea
+                  id="ingredients"
+                  type="text"
+                  name="ingredients"
+                  placeholder="Ingredients"
+                  onChange={handleChange}
+                  value={formData.ingredients}
+                />
+              </label>
+            </td>
+            <td>
+              <label htmlFor="preparation">
+                <textarea
+                  id="preparation"
+                  type="text"
+                  placeholder="Preparation"
+                  name="preparation"
+                  onChange={handleChange}
+                  value={formData.preparation}
+                />
+              </label>
+            </td>
+            <td>
+              <button type="submit">Create</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </form>
+  );
+}
+
+export default RecipeCreate;
